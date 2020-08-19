7aae1ab9f5a6e81860fc69986d1c98cd9fd55eed
jorgieo <52473778+jorgieo@users.noreply.github.com>
2020-08-19 14:29:23 -0400

    Updated method to display user stats on tab1 view

--- chorebank/src/app/tab1/tab1.page.ts
+++ chorebank/src/app/tab1/tab1.page.ts
@@ -1,20 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {UserDataService} from '../../app/user-data.service'
 import { Router } from '@angular/router';
+import { element } from 'protractor';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit{
 
   constructor(public userData: UserDataService, public router: Router) {}
+  
+  // Placeholder for user statistics
+  userStats = [];
 
-  loadUsers(){
-    return this.userData.getUser();
-  };
+  // Pull user objects once
+  ngOnInit(){
 
+    // Pull user objects and extract statistics, then return a stats object for each user
+    // and append to userStats array.
+    this.userData.getUsers().forEach(user => {
+      this.userStats.push(this.userData.getUserStats(user))
+    });
+  }
+ 
   navigate(name) {
     this.router.navigate(['user-chores'])
   }
