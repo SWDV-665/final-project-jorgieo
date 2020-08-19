b3fbba3ca85ef799dc2dbbbf21feb87222ee3c5c
jorgieo <52473778+jorgieo@users.noreply.github.com>
2020-08-19 15:46:49 -0400

    Updated imports

--- chorebank/src/app/tab1/tab1.page.ts
+++ chorebank/src/app/tab1/tab1.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {UserDataService} from '../../app/user-data.service'
 import { Router } from '@angular/router';
-import { element } from 'protractor';
+
 
 @Component({
   selector: 'app-tab1',
